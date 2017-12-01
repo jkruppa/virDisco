650a0fd2db34c49a3166c776c1bb32e465404696
jkruppa <jochen.kruppa@gmail.com>
2017-12-01 11:54:35 +0100

    sam2bam string bug

--- R/sam2bam.R
+++ R/sam2bam.R
@@ -10,11 +10,11 @@
 sam2bam_serial <- function(sam_files, keep = FALSE){
   l_ply(sam_files, function(x){
     talk("Working on ", basename(x))
-    runCMD(paste(program_list[samtools], "view -b -S", x, ">",
+    runCMD(paste(program_list["samtools"], "view -b -S", x, ">",
                  gsub(".sam", "_tmp.bam", x)))
-    runCMD(paste(program_list[samtools], "sort",
+    runCMD(paste(program_list["samtools"], "sort",
                  gsub(".sam", "_tmp.bam", x), ">", gsub(".sam", ".bam", x)))
-    runCMD(paste(program_list[samtools], "index", gsub(".sam", ".bam", x)))
+    runCMD(paste(program_list["samtools"], "index", gsub(".sam", ".bam", x)))
     if(!keep){
       unlink(c(x,
                gsub(".sam", "_tmp.bam", x),
