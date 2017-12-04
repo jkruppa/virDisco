d182c8c16ba3941f2eb18da8b4e618669e088e5d
jkruppa <jochen.kruppa@gmail.com>
2017-12-04 11:49:45 +0100

    ete3 problem with only one found species

--- R/ete3.R
+++ R/ete3.R
@@ -6,7 +6,7 @@
 ##' @param out_file 
 ##' @param pdf_file 
 ##' @param clean 
-##' @return Test
+##' @return NULL
 ##' @author Jochen Kruppa
 ##' @export
 ete2_plot <- function(tax_ids, out_file, pdf_file, clean = FALSE){
@@ -17,24 +17,23 @@ ete2_plot <- function(tax_ids, out_file, pdf_file, clean = FALSE){
   ## check if all tax ids are available and drop the unkown
   tree_info_file <- str_c(out_file, "_info.tax")
   ete3_info_cmd <- paste("cut -f1", tax_ids_full_file,
-                         "|",
-                         program_list["ete3"],
-                         "ncbiquery --info",
+                         "| ete3 ncbiquery --info",
                          ">", tree_info_file)
   runCMD(ete3_info_cmd)
   ## get the knwon tax ids into a tree 
   tree_info <- read.delim(tree_info_file)[,1]
-  tax_ids_file <- str_c(out_file, "_sub.tax")
-  write.table0(tree_info, tax_ids_file)
-  ## run the plotting
-  ete3_cmd <- paste("cut -f1", tax_ids_file,
-                    "|",
-                    program_list["ete3"],
-                    "ncbiquery --tree | xvfb-run",
-                    program_list["ete3"],
-                    "view --ncbi --image",
-                    pdf_file)
-  runCMD(ete3_cmd)
+  if(length(tree_info) > 1) {
+    tax_ids_file <- str_c(out_file, "_sub.tax")
+    write.table0(tree_info, tax_ids_file)
+    ## run the plotting
+    ete3_cmd <- paste("cut -f1", tax_ids_file,
+                      "| ete3 ncbiquery --tree | xvfb-run ete3 view --ncbi --image",
+                      pdf_file)
+    runCMD(ete3_cmd)
+  } else {
+    talk("Only one species found. Tree makes no sense -> skip")
+  }
   ## clean everything
   if(clean) unlink(list.files(dirname(out_file), ".tax$", full.names = TRUE))
 }
+## ------------------------------------------------------------
