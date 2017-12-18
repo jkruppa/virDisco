6cc2fb7c219b243bf5b59fa54d76e0aad613a1fa
jkruppa <jochen.kruppa@gmail.com>
2017-12-18 10:41:10 +0100

    the decoy approach claches with the sam index

--- R/sam2bam.R
+++ R/sam2bam.R
@@ -4,17 +4,23 @@
 ##' @title Test 
 ##' @param sam_files 
 ##' @param keep 
+##' @param index_sort 
 ##' @return NULL
 ##' @author Jochen Kruppa
 ##' @export
-sam2bam_serial <- function(sam_files, keep = FALSE){
+sam2bam_serial <- function(sam_files, keep = FALSE, index_sort = FALSE){
   l_ply(sam_files, function(x){
     talk("Working on ", basename(x))
+    if(index_sort){
     runCMD(paste(program_list["samtools"], "view -b -S", x, ">",
                  gsub(".sam", "_tmp.bam", x)))
     runCMD(paste(program_list["samtools"], "sort",
                  gsub(".sam", "_tmp.bam", x), ">", gsub(".sam", ".bam", x)))
     runCMD(paste(program_list["samtools"], "index", gsub(".sam", ".bam", x)))
+    } else {
+      runCMD(paste(program_list["samtools"], "view -b -S", x, ">",
+                   gsub(".sam", ".bam", x)))      
+    }
     if(!keep){
       unlink(c(x,
                gsub(".sam", "_tmp.bam", x),
