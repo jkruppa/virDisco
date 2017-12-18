87376c08cc8d381556de9ea5fcf17bd6be81d2ed
jkruppa <jochen.kruppa@gmail.com>
2017-12-18 11:01:22 +0100

    added samlog file

--- R/sam2bam.R
+++ R/sam2bam.R
@@ -10,16 +10,24 @@
 ##' @export
 sam2bam_serial <- function(sam_files, keep = FALSE, index_sort = FALSE){
   l_ply(sam_files, function(x){
-    talk("Working on ", basename(x))
+    talk("[SAMTOOLS] Working on ", basename(x))
     if(index_sort){
     runCMD(paste(program_list["samtools"], "view -b -S", x, ">",
-                 gsub(".sam", "_tmp.bam", x)))
+                 gsub(".sam", "_tmp.bam", x),
+                 "1>", file.path(tmpDir, "samtools.log"), "2>&1")
+           )
     runCMD(paste(program_list["samtools"], "sort",
-                 gsub(".sam", "_tmp.bam", x), ">", gsub(".sam", ".bam", x)))
-    runCMD(paste(program_list["samtools"], "index", gsub(".sam", ".bam", x)))
+                 gsub(".sam", "_tmp.bam", x), ">", gsub(".sam", ".bam", x),
+                 "1>", file.path(tmpDir, "samtools.log"), "2>&1")
+           )
+    runCMD(paste(program_list["samtools"], "index", gsub(".sam", ".bam", x),
+                 "1>", file.path(tmpDir, "samtools.log"), "2>&1")
+           )
     } else {
       runCMD(paste(program_list["samtools"], "view -b -S", x, ">",
-                   gsub(".sam", ".bam", x)))      
+                   gsub(".sam", ".bam", x),
+                   "1>", file.path(tmpDir, "samtools.log"), "2>&1")
+             )
     }
     if(!keep){
       unlink(c(x,
