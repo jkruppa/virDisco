f8635ce3a2387adccf702aedeacd641f8764b54d
jkruppa <jochen.kruppa@gmail.com>
2017-12-19 09:39:45 +0100

    changed map_dna_ref to decoy approach

--- R/map_dna_ref.R
+++ R/map_dna_ref.R
@@ -20,7 +20,7 @@ map_dna_ref <- function(infile, outfile, par_list, min_hit = 5, all = FALSE){
                                            samOutFile = gsub(".sam", "_dna.sam", outfile),
                                            referenceDir = par_list["genome_index"],
                                            method = "end-to-end",
-                                           p = 32,
+                                           p = par_list["ncore"],
                                            all)
              runCMD(bowtie2CMD)
            },
@@ -29,7 +29,7 @@ map_dna_ref <- function(infile, outfile, par_list, min_hit = 5, all = FALSE){
                runCMD(paste("gunzip -k -f", x))
              })
              star_CMD <- paste(STAR,
-                               "--runThreadN", 32,
+                               "--runThreadN", par_list["ncore"],
                                "--genomeDir", par_list["genome_index"],
                                "--readFilesIn", str_c(gsub(".gz", "", unlist(infile)), collapse = " "),
                                "--outFileNamePrefix", gsub(".sam", "_", outfile),
@@ -47,6 +47,17 @@ map_dna_ref <- function(infile, outfile, par_list, min_hit = 5, all = FALSE){
   }
   ## read in bam files
   alignment_bam <- Reduce(c, scanBam(bam_file))
+  ## get read / reference information (qname is read, rname is reference)
+  read_ref_tbl <- tibble(read = alignment_bam$qname, ref = alignment_bam$rname) %>%
+    mutate(read = str_replace(read, "R1_|R2_", "")) %>%
+    mutate(read = ifelse(str_detect(read, "decoy"), "decoy", "sample")) %>%
+    mutate(ref = ifelse(str_detect(ref, "decoy"), "decoy", "viral")) %>%
+    mutate(match = str_c(read, "_", ref))
+  ## build up the 2x2 table
+  read_ref_table <- table(reads = read_ref_tbl$read, ref = read_ref_tbl$ref)
+  multi_map_rate <- sum(read_ref_table["decoy", ]) / (2 * par_list["num_decoy_reads"])
+  true_positive <- read_ref_table["decoy", "decoy"] / sum(read_ref_table["decoy", ])
+  false_positive <- (read_ref_table["decoy", "viral"] + read_ref_table["sample", "decoy"]) / sum(read_ref_table)
   ## get the aligment data frame for plotting
   qname_raw <- str_split(alignment_bam$qname, ":", simplify = TRUE)
   alignment_raw_df <- tibble(genebank_id = alignment_bam$rname,
@@ -71,15 +82,10 @@ map_dna_ref <- function(infile, outfile, par_list, min_hit = 5, all = FALSE){
   alignment_table_clean <- alignment_table_sorted[alignment_table_sorted > min_hit]
   ## get the count data frame
   count_df <- setNames(ldply(alignment_table_clean), c("genebank_id", "read_count"))
-    ## get in the decoy information
-  decoy_pos <- str_detect(count_df$genebank_id, "decoy")
-  false_positive <- count_df[decoy_pos,]$read_count / sum(count_df[!decoy_pos,]$read_count)
-  ## adjusted count by false positives
-  count_df %<>% 
-    mutate(read_count_adj = round(read_count * false_positive)) %>%
-    extract(!decoy_pos,)
   ## return list with counts and aligment information
   return(list(count_df = select(count_df, genebank_id, count_read = read_count),
               alignment_df = alignment_df,
-              false_positive = false_positive))
+              stats = c(multi_map_rate = multi_map_rate,
+                        true_positive = true_positive,
+                        false_positive = false_positive)))
 }
