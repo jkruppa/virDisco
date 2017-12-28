b5f27b31003fc62652be589b45ee870513f75e74
jkruppa <jochen.kruppa@gmail.com>
2017-12-28 10:49:25 +0100

    updated random seq

--- R/random_dna_seq.R
+++ R/random_dna_seq.R
@@ -7,32 +7,41 @@
 ##' @param k 
 ##' @param n 
 ##' @param clean 
+##' @param tmpDir 
+##' @param fasta_formatter 
+##' @param fasta_uShuffle 
 ##' @return Null
 ##' @author Jochen Kruppa
 ##' @export
-random_dna_seq <- function(inFile, outFile, k, n = 1, clean = TRUE){
-    ## both needed programs
+random_dna_seq <- function(inFile, outFile, k, n = 1, clean = TRUE,
+                           tmpDir, fasta_formatter = NULL,
+                           fasta_uShuffle = NULL){
+  ## both needed programs
+  if(is.null(fasta_formatter)){
     fasta_formatter <- file.path(programDir, "fastx-Toolkit/bin/fasta_formatter")
+  }
+  if(is.null(fasta_uShuffle)) {
     fasta_uShuffle <- file.path(programDir, "fasta_ushuffle/fasta_ushuffle")
-    ## build up tmpFile
-    tmpFile <- file.path(tmpDir, gsub(".fasta|.fa", "_tmp.shuffle", basename(inFile)))
-    ## build up commands: first fasta with no line breaks
-    formatterCMD <- paste(fasta_formatter,
-                          "-i", inFile,
-                          "-o", tmpFile)
-    runCMD(formatterCMD)
-    ## build up commands: second shuffle the tmp file
-    uShuffleCMD <- paste(fasta_uShuffle,
-                         "<", tmpFile,
-                         "-n", n,
-                         "-k", k,
-                         "-r", 1000,
-                         ## "-seed", 20160916,
-                         ">", outFile,
-                         "2>", file.path(tmpDir, "shuffle.log"))
-    runCMD(uShuffleCMD)
-    ## clean...
-    if(clean){
-        file.remove(tmpFile)
-    }
+  }
+  ## build up tmpFile
+  tmpFile <- file.path(tmpDir, gsub(".fasta|.fa", "_tmp.shuffle", basename(inFile)))
+  ## build up commands: first fasta with no line breaks
+  formatterCMD <- paste(fasta_formatter,
+                        "-i", inFile,
+                        "-o", tmpFile)
+  runCMD(formatterCMD)
+  ## build up commands: second shuffle the tmp file
+  uShuffleCMD <- paste(fasta_uShuffle,
+                       "<", tmpFile,
+                       "-n", n,
+                       "-k", k,
+                       "-r", 1000,
+                       ## "-seed", 20160916,
+                       ">", outFile,
+                       "2>", file.path(tmpDir, "shuffle.log"))
+  runCMD(uShuffleCMD)
+  ## clean...
+  if(clean){
+    file.remove(tmpFile)
+  }
 }
