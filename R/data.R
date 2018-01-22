97fd61150de96c28920905ea0e4c320babfb9fd1
jkruppa <jochen.kruppa@gmail.com>
2018-01-22 16:07:51 +0100

    Finished first round of helping pages...

--- R/data.R
+++ R/data.R
@@ -1,4 +1,4 @@
-#' Dataset
+#' Output of ord_findings
 #'
 #' A example output of \code{\link{ord_findings}}.
 #'
@@ -13,7 +13,7 @@
 #' @source \url{ftp://ftp.ncbi.nlm.nih.gov/genbank}
 "NHS_10001_count_tbl"
 
-#' Dataset
+#' Output of map_pep_ref
 #'
 #' A example output of \code{\link{map_pep_ref}}.
 #'
@@ -38,28 +38,78 @@
 "NHS_10001_map_aa_list"
 
 
-#' Dataset
+#' Output of get_consensus_df
 #'
-#' Test
+#' A example output of the \code{\link{get_consensus_df}} function.
 #'
 #' @format A list with two entries:
+#'
+#' $consensus_df
 #' \describe{
-#'   \item{R1}{Fastq reads object with 10000 paired reads of R1}
-#'   \item{R2}{Fastq reads object with 10000 paired reads of R2}
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{con_prob}{Coverage probability. How unique is the base?}
+#'   \item{pos}{Position on the reference}
+#'   \item{bool}{Mathc between the reference and the consensus sequence}
+#' }
+#'
+#' $entropy_df
+#' \describe{
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{entropy}{Entropy of the consensus reference}
+#' }
+#'
+#' $run_len_df
+#' \describe{
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{min}{Minimum run of bases to the reference}
+#'   \item{first}{First quartile of the runs to the reference}
+#'   \item{median}{Median of the runs to the reference}
+#'   \item{mean}{Mean of the runs to the reference}
+#'   \item{third}{Third quartile of the runs to the reference}
+#'   \item{max}{Maximum run of bases to the reference}
+#'   \item{run_to_ref}{Maximal run to the reference}
+#'   \item{run_not_to_ref}{Maximal run _not_ to the reference}
+#' }
+#'
+#' $median_con_prob_df
+#' \describe{
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{min}{Minimum of the consensus_df$con_prob}
+#'   \item{first}{First quartile of the consensus_df$con_prob}
+#'   \item{median}{Median of the consensus_df$con_prob}
+#'   \item{mean}{Mean of the consensus_df$con_prob}
+#'   \item{third}{Third quartile of the consensus_df$con_prob}
+#'   \item{max}{Maximum of the consensus_df$con_prob}
+#' }
+#'
+#'$coverage_equal_ref_df
+#' \describe{
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{coverage_equal_ref}{How many of the consensus base match the reference base?}
+#' }
+#'
+#' $coverage_overall_ref_df
+#' \describe{
+#'   \item{genebank_id}{Genbank ID of the reference viral strain}
+#'   \item{coverage_overall_ref}{Ho much of the reference is covered by the consensus sequence}
 #' }
 #' @source \url{ftp://ftp.ncbi.nlm.nih.gov/genbank}
 "NHS_10001_consensus"
 
-#' Dataset
+
+
+#' Output of map_dna_ref
 #'
 #' A example output of \code{\link{map_dna_ref}}.
 #'
 #' @format A list with two data.frame:
+#' 
 #' $count_df
 #' \describe{
 #'   \item{genebank_id}{Genbank reference ID}
 #'   \item{count_read}{Number of reads mapped to the genebank ID reference}
 #' }
+#' 
 #' $alignment_df
 #' \describe{
 #'   \item{genebank_id}{Genbank reference ID}
@@ -75,7 +125,7 @@
 "NHS_10001_map_dna_list"
 
 
-#' Dataset
+#' Example fastq files
 #'
 #' A list with two fastq objects including R1 and R2 reads
 #'
@@ -88,7 +138,7 @@
 "infected_fastq"
 
 
-#' Dataset
+#' Example species infro db
 #'
 #' A dataset containing 287 viral strains and information
 #'
@@ -106,7 +156,7 @@
 #' @source \url{ftp://ftp.ncbi.nlm.nih.gov/genbank}
 "strain_info_db"
 
-#' Dataset
+#' Example amino acid information db
 #'
 #' A dataset containing 519 protein IDs, the start and the end position
 #' on the DNA sequence
